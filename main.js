d613f2d802b2510f50fed17c1fd74e9467a1f44c
Emmanuel Santiago <biosinx@gmail.com>
2021-01-21 21:11:24 -0500

    funciones_21_01_2021

---
+++ main.js
@@ -0,0 +1,18 @@
+// Funcion bienvenida
+function inicio(p1){
+    console.log(p1);
+}
+let data_us = prompt('Escribe tu nombre');
+inicio(data_us);
+
+// Funcion despedida
+function final(){
+    console.log('Adios');
+}
+final()
+
+//Funcion cierre
+function cierre(){
+    alert('Cerrando');
+}
+cierre()
